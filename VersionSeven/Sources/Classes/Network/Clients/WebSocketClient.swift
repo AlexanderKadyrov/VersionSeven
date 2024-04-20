c9c1fa20e4a36cc5ed30789ab4c5d0398f67e19e
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-21 03:31:05 +0500

    fixes

--- VersionSeven/Sources/Classes/Network/Clients/WebSocketClient.swift
+++ VersionSeven/Sources/Classes/Network/Clients/WebSocketClient.swift
@@ -6,8 +6,6 @@ final class WebSocketClient {
     private let socket: WebSocket
     
     init() {
-        //wss://a.nel.cloudflare.ru
-        //wss://wss.tradernet.ru
         var request = URLRequest(url: URL(string: "wss://wss.tradernet.com")!)
         request.timeoutInterval = 10
         socket = WebSocket(request: request)
@@ -62,35 +60,6 @@ extension WebSocketClient: WebSocketDelegate {
             break
         case .peerClosed:
             break
-        }
-    }
-}
-
-enum List<O: Codable>: Codable {
-    
-    case string(String)
-    case object(O)
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        do {
-            self = try .string(container.decode(String.self))
-        } catch DecodingError.typeMismatch {
-            do {
-                self = try .object(container.decode(O.self))
-            } catch DecodingError.typeMismatch {
-                throw DecodingError.typeMismatch(Self.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Encoded payload not of an expected type"))
-            }
-        }
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        switch self {
-        case .string(let value):
-            try container.encode(value)
-        case .object(let value):
-            try container.encode(value)
         }
     }
 }
