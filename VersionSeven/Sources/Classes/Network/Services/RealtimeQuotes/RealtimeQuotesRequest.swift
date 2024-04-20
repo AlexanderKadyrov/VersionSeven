99371d1d5873391c0618d4fc64436b18fcc972e8
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-21 03:26:39 +0500

    add new files

---
+++ VersionSeven/Sources/Classes/Network/Services/RealtimeQuotes/RealtimeQuotesRequest.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct RealtimeQuotesRequest: Codable {
+    let realtimeQuotes: [String]
+}
---
+++ VersionSeven/Sources/Classes/Network/Services/RealtimeQuotes/RealtimeQuotesResponse.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+struct RealtimeQuotesResponse: Codable {
+    
+    /// Тикер
+    let c: String
+    
+    /// Изменение в процентах относительно цены закрытия предыдущей торговой сессии
+    let pcp: Float
+    
+    /// Биржа последней сделки
+    let ltr: String
+    
+    /// Название бумаги
+    let name: String
+    
+    /// Цена последней сделки
+    let ltp: Float
+    
+    /// Изменение цены последней сделки в пунктах относительно цены закрытия предыдущей торговой сессии
+    let chg: Float
+}
