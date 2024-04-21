13e6a39f5993a27b735523f1f88169eeaa6f678b
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-22 01:11:59 +0500

    fixes

--- VersionSeven/Sources/Classes/ViewControllers/Quotes/Cells/QuotesTabloidCellViewModel.swift
+++ VersionSeven/Sources/Classes/ViewControllers/Quotes/Cells/QuotesTabloidCellViewModel.swift
@@ -8,8 +8,10 @@ final class QuotesTabloidCellViewModel: TabloidCellViewModel {
     }
     
     let quotes: Quotes
+    let text: String
     
     init(quotes: Quotes) {
+        self.text = [quotes.ltr, quotes.name].joined(separator: " | ")
         self.quotes = quotes
         super.init(cellIdentifier: Constants.cellIdentifier)
     }
