e849aafa1315b7e70db4665284b15a05d3fafa54
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-21 23:42:08 +0500

    fixes

--- VersionSeven/Sources/Classes/ViewControllers/Quotes/Cells/QuotesTabloidCellView.swift
+++ VersionSeven/Sources/Classes/ViewControllers/Quotes/Cells/QuotesTabloidCellView.swift
@@ -4,6 +4,19 @@ import UIKit
 
 final class QuotesTabloidCellView: TabloidCellView {
     
+    private lazy var verticalStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [cLabel])
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .vertical
+        return view
+    }()
+    
+    private let cLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         configureViews()
@@ -17,9 +30,19 @@ final class QuotesTabloidCellView: TabloidCellView {
     private func configureViews() {
         backgroundColor = .white
         selectionStyle = .none
+        
+        contentView.addSubview(verticalStackView)
+        NSLayoutConstraint.activate([
+            verticalStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            verticalStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            verticalStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            verticalStackView.topAnchor.constraint(equalTo: contentView.topAnchor)
+        ])
     }
     
     override func fill(viewModel: TabloidCellViewModel?) {
         super.fill(viewModel: viewModel)
+        guard let viewModel = viewModel as? QuotesTabloidCellViewModel else { return }
+        cLabel.text = viewModel.quotes.c
     }
 }
