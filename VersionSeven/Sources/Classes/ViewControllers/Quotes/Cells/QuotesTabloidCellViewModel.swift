f4336b3f163d82629156d3e6d3a321780d675b17
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-22 03:33:18 +0500

    fixes

--- VersionSeven/Sources/Classes/ViewControllers/Quotes/Cells/QuotesTabloidCellViewModel.swift
+++ VersionSeven/Sources/Classes/ViewControllers/Quotes/Cells/QuotesTabloidCellViewModel.swift
@@ -1,18 +1,26 @@
 import TabloidView
 import Foundation
 
-final class QuotesTabloidCellViewModel: TabloidCellViewModel {
+final class QuotesTabloidCellViewModel: TabloidCellViewModel, Hashable {
     
     enum Constants {
         static let cellIdentifier = "QuotesTabloidCellView"
     }
     
-    let quotes: Quotes
+    let quote: Quote
     let text: String
     
-    init(quotes: Quotes) {
-        self.text = [quotes.ltr, quotes.name].joined(separator: " | ")
-        self.quotes = quotes
+    init(quote: Quote) {
+        self.text = [quote.ltr, quote.name].joined(separator: " | ")
+        self.quote = quote
         super.init(cellIdentifier: Constants.cellIdentifier)
+    }
+    
+    static func == (lhs: QuotesTabloidCellViewModel, rhs: QuotesTabloidCellViewModel) -> Bool {
+        return lhs.quote == rhs.quote
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(quote)
     }
 }
