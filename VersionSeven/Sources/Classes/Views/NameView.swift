9f70bf651931325256b70d871bb2d53d661f6e92
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-22 00:50:10 +0500

    fixes

---
+++ VersionSeven/Sources/Classes/Views/NameView.swift
@@ -0,0 +1,38 @@
+import Foundation
+import UIKit
+
+final class NameView: UIView {
+    
+    private let textLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    var text: String? {
+        set { textLabel.text = newValue }
+        get { return textLabel.text }
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureViews()
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        configureViews()
+    }
+    
+    private func configureViews() {
+        addSubview(textLabel)
+        NSLayoutConstraint.activate([
+            textLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            textLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            textLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            textLabel.topAnchor.constraint(equalTo: topAnchor)
+        ])
+    }
+}
