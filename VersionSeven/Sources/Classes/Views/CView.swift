dfcb69c26aec9d793eadf4488c7793649d86c8eb
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-22 00:48:07 +0500

    fixes

---
+++ VersionSeven/Sources/Classes/Views/CView.swift
@@ -0,0 +1,36 @@
+import Foundation
+import UIKit
+
+final class CView: UIView {
+    
+    private let textLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var text: String? {
+        set { textLabel.text = newValue }
+        get { return textLabel.text }
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureViews()
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        configureViews()
+    }
+    
+    private func configureViews() {
+        addSubview(textLabel)
+        NSLayoutConstraint.activate([
+            textLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            textLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            textLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            textLabel.topAnchor.constraint(equalTo: topAnchor)
+        ])
+    }
+}
