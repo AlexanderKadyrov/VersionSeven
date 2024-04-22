ab6bb0231a1f50aa38722d6c4aa76d5d3108027f
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-22 17:56:10 +0500

    fixes

---
+++ VersionSeven/Sources/Classes/Extensions/Array+.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+extension Array {
+    func sorted<T: Hashable>(like otherArray: [T], keyPath: KeyPath<Element, T>) -> [Element] {
+        let dict = otherArray.enumerated().reduce(into: [:]) { $0[$1.element] = $1.offset }
+        return sorted {
+            guard let thisIndex = dict[$0[keyPath: keyPath]] else { return false }
+            guard let otherIndex = dict[$1[keyPath: keyPath]] else { return true }
+            return thisIndex < otherIndex
+        }
+    }
+}
