6b5a3fb93f25b4ed0c6846d8b5b5395d0a6dea60
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-22 17:59:05 +0500

    fixes

--- VersionSeven/Sources/Classes/ViewControllers/Quotes/QuotesViewModel.swift
+++ VersionSeven/Sources/Classes/ViewControllers/Quotes/QuotesViewModel.swift
@@ -3,8 +3,43 @@ import Foundation
 
 final class QuotesViewModel {
     
+    private let identifiers = [
+        "SP500.IDX",
+        "AAPL.US",
+        "RSTI",
+        "GAZP",
+        "MRKZ",
+        "RUAL",
+        "HYDR",
+        "MRKS",
+        "SBER",
+        "FEES",
+        "TGKA",
+        "VTBR",
+        "ANH.US",
+        "VICL.US",
+        "BURG.US",
+        "NBL.US",
+        "YETI.US",
+        "WSFS.US",
+        "NIO.US",
+        "DXC.US",
+        "MIC.US",
+        "HSBC.US",
+        "EXPN.EU",
+        "GSK.EU",
+        "SHP.EU",
+        "MAN.EU",
+        "DB1.EU",
+        "MUV2.EU",
+        "TATE.EU",
+        "KGF.EU",
+        "MGGT.EU",
+        "SGGD.EU"
+    ]
+    
     private lazy var quotesService: QuotesService = {
-        let service = QuotesService()
+        let service = QuotesService(identifiers: identifiers)
         service.delegate = self
         return service
     }()
@@ -21,13 +56,14 @@ extension QuotesViewModel: QuotesServiceDelegate {
         let cellViewModels = quotes.map { QuotesTabloidCellViewModel(quote: $0) }
         let new = Set(cellViewModels)
         
-        let sections = tabloidViewModel.sections
+        let oldSections = tabloidViewModel.sections
             .flatMap { $0 }
             .compactMap { $0 as? QuotesTabloidCellViewModel }
         
-        let old = Set(sections)
-        let set = new.union(old)
+        let old = Set(oldSections)
+        let newSections = Array(new.union(old))
+        let sorted = newSections.sorted(like: identifiers, keyPath: \.quote.c)
         
-        tabloidViewModel.sections = [Array(set)]
+        tabloidViewModel.sections = [sorted]
     }
 }
