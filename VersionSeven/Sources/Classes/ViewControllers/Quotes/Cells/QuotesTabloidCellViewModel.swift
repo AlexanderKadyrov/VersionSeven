3d53ab9cefae0ceaef8d54b4c6c4a9c8063f3aeb
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-22 17:14:45 +0500

    fixes

--- VersionSeven/Sources/Classes/ViewControllers/Quotes/Cells/QuotesTabloidCellViewModel.swift
+++ VersionSeven/Sources/Classes/ViewControllers/Quotes/Cells/QuotesTabloidCellViewModel.swift
@@ -7,20 +7,36 @@ final class QuotesTabloidCellViewModel: TabloidCellViewModel, Hashable {
         static let cellIdentifier = "QuotesTabloidCellView"
     }
     
+    private(set) var ltp: LTP
+    
     let quote: Quote
     let text: String
     
     init(quote: Quote) {
         self.text = [quote.ltr, quote.name].joined(separator: " | ")
+        self.ltp = .equal(quote.ltp)
         self.quote = quote
         super.init(cellIdentifier: Constants.cellIdentifier)
     }
     
     static func == (lhs: QuotesTabloidCellViewModel, rhs: QuotesTabloidCellViewModel) -> Bool {
-        return lhs.quote == rhs.quote
+        if lhs.quote.c == rhs.quote.c {
+            if lhs.quote.ltp > rhs.quote.ltp {
+                lhs.ltp = .up(lhs.quote.ltp)
+                rhs.ltp = .down(rhs.quote.ltp)
+            } else if lhs.quote.ltp < rhs.quote.ltp {
+                lhs.ltp = .down(lhs.quote.ltp)
+                rhs.ltp = .up(rhs.quote.ltp)
+            } else {
+                lhs.ltp = .equal(lhs.quote.ltp)
+                rhs.ltp = .equal(rhs.quote.ltp)
+            }
+            return true
+        }
+        return false
     }
     
     func hash(into hasher: inout Hasher) {
-        hasher.combine(quote)
+        hasher.combine(quote.c)
     }
 }
