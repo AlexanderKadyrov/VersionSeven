16d733c4c12be41114077d88cbc1c2d9004bef1e
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-23 03:46:00 +0500

    work with LTP

--- VersionSeven/Sources/Classes/Views/LTPView/LTP.swift
+++ VersionSeven/Sources/Classes/Views/LTPView/LTP.swift
@@ -1,7 +1,37 @@
 import Foundation
 
-enum LTP {
+enum LTP: Codable, Comparable {
+    
     case equal(Float)
     case down(Float)
     case up(Float)
+    
+    var rawValue: Float {
+        switch self {
+        case .equal(let value):
+            return value
+        case .down(let value):
+            return value
+        case .up(let value):
+            return value
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let value = try container.decode(Float.self)
+        self = .equal(value)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .equal(let value):
+            try container.encode(value)
+        case .down(let value):
+            try container.encode(value)
+        case .up(let value):
+            try container.encode(value)
+        }
+    }
 }
