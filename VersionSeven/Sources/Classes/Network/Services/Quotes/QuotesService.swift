ed6874641f5ea242252a6360dd0b24ecff12272f
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-23 02:08:32 +0500

    fixes

--- VersionSeven/Sources/Classes/Network/Services/Quotes/QuotesService.swift
+++ VersionSeven/Sources/Classes/Network/Services/Quotes/QuotesService.swift
@@ -35,23 +35,25 @@ extension QuotesService: WebSocketClientDelegate {
     
     func didReceive(data: Data, webSocketClient: WebSocketClient) {
         do {
-            let newValue = try JSON(data: data)
-            if let q = newValue[0].string, q == "q" {
-                let newObject = newValue[1]
-                let newData = try newObject.rawData()
-                let newQuote = try JSONDecoder().decode(Quote.self, from: newData)
-                if let oldQuote = quotes.first(where: { $0.c == newQuote.c }) {
-                    let oldData = try JSONEncoder().encode(oldQuote)
-                    let oldObject = try JSON(data: oldData)
-                    let merged = try oldObject.merged(with: newObject)
-                    let mergedData = try merged.rawData()
-                    let mergedQuote = try JSONDecoder().decode(Quote.self, from: mergedData)
-                    quotes = Set([mergedQuote]).union(quotes)
-                } else {
-                    quotes = Set([newQuote]).union(quotes)
+            let list = try JSONDecoder().decode([List<Quote>].self, from: data)
+            let result = list.compactMap { result in
+                if case let .object(value) = result {
+                    return value
                 }
-                delegate?.didReceive(quotes: Array(quotes))
+                return nil
             }
+            
+            for newQuote in result {
+                if let oldQuote = quotes.first(where: { $0.c == newQuote.c }) {
+                    let mergedQuote = try oldQuote.merged(with: newQuote)
+                    quotes.remove(oldQuote)
+                    quotes.insert(mergedQuote)
+                } else {
+                    quotes.insert(newQuote)
+                }
+            }
+            
+            delegate?.didReceive(quotes: Array(quotes))
         } catch {
             
         }
