a3b55a0fe2074e77747362b3b840e27cf13b50b4
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-25 01:29:44 +0500

    fixes

--- VersionSeven/Sources/Classes/Views/Image/ImageView.swift
+++ VersionSeven/Sources/Classes/Views/Image/ImageView.swift
@@ -1,6 +1,38 @@
 import Foundation
+import Kingfisher
 import UIKit
 
 final class ImageView: UIView {
     
+    private let imageView: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    var imageViewModel: ImageViewModel? {
+        didSet {
+            imageView.kf.setImage(with: imageViewModel?.url)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureViews()
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        configureViews()
+    }
+    
+    private func configureViews() {
+        addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            imageView.topAnchor.constraint(equalTo: topAnchor)
+        ])
+    }
 }
--- VersionSeven/Sources/Classes/Views/Image/ImageViewModel.swift
+++ VersionSeven/Sources/Classes/Views/Image/ImageViewModel.swift
@@ -2,4 +2,9 @@ import Foundation
 
 final class ImageViewModel {
     
+    let url: URL?
+    
+    init?(url: URL?) {
+        self.url = url
+    }
 }
