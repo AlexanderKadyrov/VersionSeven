69c0f1683b86730bca14468c2d887c995cd55fac
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-26 04:24:14 +0500

    work with StocksViewController

--- VersionSeven/Sources/Classes/ViewControllers/Stocks/StocksViewController.swift
+++ VersionSeven/Sources/Classes/ViewControllers/Stocks/StocksViewController.swift
@@ -17,6 +17,10 @@ final class StocksViewController: UIViewController {
         return view
     }()
     
+    private var hasEnabledOneStock: Bool {
+        return viewModel?.hasEnabledOneStock ?? false
+    }
+    
     var viewModel: StocksViewModel? {
         didSet {
             tabloidView.viewModel = viewModel?.tabloidViewModel
@@ -32,6 +36,27 @@ final class StocksViewController: UIViewController {
     
     private func configureToolbar() {
         title = "Stocks"
+        configureDone()
+        if hasEnabledOneStock {
+            configureUndo()
+        } else {
+            configureRedo()
+        }
+    }
+    
+    private func configureUndo() {
+        let barButtonItem = UIBarButtonItem(barButtonSystemItem: .undo, target: self, action: #selector(actionUndo))
+        barButtonItem.tintColor = .black
+        navigationItem.leftBarButtonItems = [barButtonItem]
+    }
+    
+    private func configureRedo() {
+        let barButtonItem = UIBarButtonItem(barButtonSystemItem: .redo, target: self, action: #selector(actionRedo))
+        barButtonItem.tintColor = .black
+        navigationItem.leftBarButtonItems = [barButtonItem]
+    }
+    
+    private func configureDone() {
         let barButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(actionDone))
         barButtonItem.tintColor = .black
         navigationItem.rightBarButtonItems = [barButtonItem]
@@ -47,6 +72,18 @@ final class StocksViewController: UIViewController {
             tabloidView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tabloidView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
         ])
+    }
+    
+    @objc
+    private func actionUndo() {
+        configureRedo()
+        viewModel?.actionUndo()
+    }
+    
+    @objc
+    private func actionRedo() {
+        configureUndo()
+        viewModel?.actionRedo()
     }
     
     @objc
