d1c326952f52608a8c40e8c874fc992645cc5969
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-26 00:52:13 +0500

    fixes

--- VersionSeven/Sources/Classes/ViewControllers/Stocks/StocksViewController.swift
+++ VersionSeven/Sources/Classes/ViewControllers/Stocks/StocksViewController.swift
@@ -1,6 +1,56 @@
+import TabloidView
 import Foundation
 import UIKit
 
 final class StocksViewController: UIViewController {
     
+    private lazy var tabloidView: TabloidView = {
+        let view = TabloidView(style: .plain)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.register(cellIdentifiers: ["StocksTabloidCellView"])
+        view.contentInsetAdjustmentBehavior = .never
+        view.separatorStyle = .singleLine
+        view.backgroundColor = .white
+        if #available(iOS 15.0, *) {
+            view.sectionHeaderTopPadding = .zero
+        }
+        return view
+    }()
+    
+    var viewModel: StocksViewModel? {
+        didSet {
+            tabloidView.viewModel = viewModel?.tabloidViewModel
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureToolbar()
+        configureViews()
+        viewModel?.viewDidLoad()
+    }
+    
+    private func configureToolbar() {
+        title = "Stocks"
+        let barButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(actionDone))
+        barButtonItem.tintColor = .black
+        navigationItem.rightBarButtonItems = [barButtonItem]
+    }
+    
+    private func configureViews() {
+        edgesForExtendedLayout = []
+        view.backgroundColor = .white
+        view.addSubview(tabloidView)
+        NSLayoutConstraint.activate([
+            tabloidView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            tabloidView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            tabloidView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tabloidView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        ])
+    }
+    
+    @objc
+    private func actionDone() {
+        viewModel?.actionDone()
+    }
 }
--- VersionSeven/Sources/Classes/ViewControllers/Stocks/StocksViewModel.swift
+++ VersionSeven/Sources/Classes/ViewControllers/Stocks/StocksViewModel.swift
@@ -1,5 +1,15 @@
+import TabloidView
 import Foundation
 
 final class StocksViewModel {
     
+    let tabloidViewModel = TabloidViewModel()
+    
+    func viewDidLoad() {
+        
+    }
+    
+    func actionDone() {
+        
+    }
 }
