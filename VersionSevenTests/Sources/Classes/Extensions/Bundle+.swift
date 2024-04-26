8a6d946de22b0db486e077d2ca30a5b0ae3db535
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-27 01:19:54 +0500

    fixes

--- VersionSevenTests/Sources/Classes/Extensions/Bundle+.swift
+++ VersionSevenTests/Sources/Classes/Extensions/Bundle+.swift
@@ -3,7 +3,7 @@ import Foundation
 extension Bundle {
     
     func file(name: String, type: String) -> Data? {
-        guard let path = Bundle.main.path(forResource: name, ofType: type) else { return nil }
+        guard let path = path(forResource: name, ofType: type) else { return nil }
         let data = try? Data(contentsOf: URL(fileURLWithPath: path))
         return data
     }
