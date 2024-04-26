f3d22b5f745b454978f5634ddbca40ac1315550c
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-26 23:38:43 +0500

    fixes

--- VersionSeven/Sources/Classes/Network/Clients/HTTP/HTTPClient.swift
+++ VersionSeven/Sources/Classes/Network/Clients/HTTP/HTTPClient.swift
@@ -4,6 +4,27 @@ final class HTTPClient {
     
     private enum Errors: Error {
         case undefined
+        case urlEmpty
+    }
+    
+    func send<P: Codable>(source: String, query: Query<P>, completion: ((Result<Data, Error>) -> ())?) {
+        guard var components = URLComponents(string: source) else {
+            completion?(.failure(Errors.urlEmpty))
+            return
+        }
+        do {
+            let data = try JSONEncoder().encode(query)
+            let q = String(decoding: data, as: UTF8.self)
+            components.queryItems = [URLQueryItem(name: "q", value: q)]
+            guard let url = components.url else {
+                completion?(.failure(Errors.urlEmpty))
+                return
+            }
+            let request = URLRequest(url: url)
+            send(request: request, completion: completion)
+        } catch {
+            completion?(.failure(Errors.undefined))
+        }
     }
     
     func send(request: URLRequest, completion: ((Result<Data, Error>) -> ())?) {
