f8001c7d1436818bc04256bf9cf19b0080e2f22e
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-27 00:14:06 +0500

    fixes

--- VersionSeven/Sources/Classes/ViewControllers/Stocks/Cells/StockTabloidCellViewModel.swift
+++ VersionSeven/Sources/Classes/ViewControllers/Stocks/Cells/StockTabloidCellViewModel.swift
@@ -7,10 +7,19 @@ final class StockTabloidCellViewModel: TabloidCellViewModel {
         static let cellIdentifier = "StockTabloidCellView"
     }
     
+    override var differenceIdentifier: String {
+        return stock.ticker
+    }
+    
     let stock: Stock
     
     init(stock: Stock) {
         self.stock = stock
         super.init(cellIdentifier: Constants.cellIdentifier)
+    }
+    
+    override func isContentEqual(to source: TabloidCellViewModel) -> Bool {
+        guard let source = source as? Self else { return false }
+        return source.stock == stock
     }
 }
