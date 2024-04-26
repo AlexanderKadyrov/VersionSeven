94b851df88b52038ff9c6fb9996b285fc28a65bc
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-27 00:46:22 +0500

    fixes

---
+++ VersionSeven/Sources/Classes/Network/Services/Quotes/QuotesFactory.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+final class QuotesFactory {
+    
+    private var quotes = Set<Quote>()
+    
+    func quotes(data: Data) -> [Quote] {
+        do {
+            let list = try JSONDecoder().decode([List<Quote>].self, from: data)
+            let result = list.compactMap { result in
+                if case let .object(value) = result {
+                    return value
+                }
+                return nil
+            }
+            
+            for newQuote in result {
+                if let oldQuote = quotes.first(where: { $0.c == newQuote.c }) {
+                    let mergedQuote = oldQuote.merged(with: newQuote)
+                    quotes.remove(oldQuote)
+                    quotes.insert(mergedQuote)
+                } else {
+                    quotes.insert(newQuote)
+                }
+            }
+        } catch {
+            
+        }
+        return Array(quotes)
+    }
+}
