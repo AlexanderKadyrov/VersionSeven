6152085341339f2466a6acc304c4340db9f19c76
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-27 00:00:24 +0500

    fixes

--- VersionSeven/Sources/Classes/Network/Services/Stocks/StocksService.swift
+++ VersionSeven/Sources/Classes/Network/Services/Stocks/StocksService.swift
@@ -2,4 +2,26 @@ import Foundation
 
 final class StocksService {
     
+    private enum Constants {
+        static let source = "https://tradernet.com/api/"
+    }
+    
+    private let httpClient = HTTPClient()
+    
+    func fetch(query: Query<ParamsStocks>, completion: ((Result<[Stock], Error>) -> ())?) {
+        httpClient.send(source: Constants.source, query: query) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let response = try JSONDecoder().decode(StocksResponse.self, from: data)
+                    let stocks = response.tickers.map { Stock(ticker: $0) }
+                    completion?(.success(stocks))
+                } catch {
+                    completion?(.failure(error))
+                }
+            case .failure(let error):
+                completion?(.failure(error))
+            }
+        }
+    }
 }
